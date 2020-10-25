60b212a42f73b6e5bc5351dd3e4fefe0102a44df
Ryan Wu <imgreenwu@gmail.com>
2020-10-26 00:46:00 +0800

    fixed hexToFigmaRgb method

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,14 @@
+const figmaColorKeys = ['r', 'g', 'b']
+
 export const hexToFigmaRgb = hex => hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
     .substring(1).match(/.{2}/g)
     .map(x => parseInt(x, 16)/255)
+    .reduce((figmaColor: RGB, value, index) => {
+
+        figmaColor[figmaColorKeys[index]] = value
+
+        return figmaColor
+    }, {})
 
 export const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
         const hex = x.toString(16)
