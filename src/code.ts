de9ad51eeaa9750ddb896f80147a704cc72848f8
Ryan Wu <imgreenwu@gmail.com>
2020-10-26 13:05:15 +0800

    updated plugin height

--- src/code.ts
+++ src/code.ts
@@ -10,7 +10,7 @@ const regExpSet: Record<string, RegExp> = {
     digits: /[0-9]+/g
 }
 
-figma.showUI(__html__, {width: 260, height: 340 });
+figma.showUI(__html__, {width: 260, height: 370 });
 
 figma.listAvailableFontsAsync().then(result => {
 	figma.ui.postMessage({
